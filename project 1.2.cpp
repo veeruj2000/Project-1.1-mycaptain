856c8cd46e6a09f0d950c3dea1aaafc665d5940c
UJWAL PANKAJ JAIN <57255611+veeruj2000@users.noreply.github.com>
2020-09-16 00:53:06 +0530

    Add files via upload

---
+++ project 1.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+	double a, b, res;
+	cout<< " Enter two numbers: ";
+	
+	cin >> a >> b;
+	
+	res = a * b;
+	cout<< "Product= " << res;
+	return 0;
+}
